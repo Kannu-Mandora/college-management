14da00e4dedc3386b9dd42ab0bcdce854c6f1351
Kannu Mandora <mandorakannu@gmail.com>
2023-06-30 13:18:51 +0530

    Fix: Allow Github Avatar Visible in welcome page.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['ik.imagekit.io', "lh3.googleusercontent.com"],
-    },
-    webpack(config) {
-      config.experiments = { ...config.experiments, topLevelAwait: true };
-      return config;
-  }
-  }
-  
-  module.exports = nextConfig
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: [
+      "ik.imagekit.io",
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+    ],
+  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
